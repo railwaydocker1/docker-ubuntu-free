d8575de453f23dbad5f56ac83f840277a7d4e09f
Morteza Kehtari <morteza.kehtari@gmail.com>
2026-09-10 12:24:59 +0330

    added other protocols

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# پورت Xray روی همون پورتی که Railway می‌ده (default 8080)
 XRAY_PORT=${PORT:-8080}
+# فقط اولین پورت (VLESS) را تغییر می‌دهیم تا پورت‌های داخلی Fallback دست‌نخورده باقی بمانند
+sed -i "s/\"port\": 8080/\"port\": $XRAY_PORT/" /usr/local/etc/xray/config.json
 
-# جایگزین کردن پورت در config.json (با یک Regex مطمئن که در ری‌استارت‌های کانتینر هم درست کار کند)
-sed -i -E "s/\"port\":[[:space:]]*[0-9]+/\"port\": $XRAY_PORT/g" /usr/local/etc/xray/config.json
-
-# اجرای Xray به صورت foreground (پروسه اصلی کانتینر که نباید متوقف شود)
 exec xray run -c /usr/local/etc/xray/config.json
